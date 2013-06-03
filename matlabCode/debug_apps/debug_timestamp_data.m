5b4d915458263fed0f599a2d03ce80b80c47b6b1
Felix Achilles <mor1pheus@gmail.com>
2013-06-03 15:51:32 +0200

    debug timestamp

--- matlabCode/debug_apps/debug_timestamp_data.m
+++ matlabCode/debug_apps/debug_timestamp_data.m
@@ -50,16 +50,16 @@ for j = 1:numFiles %equals amount of OT-files (each file represents several meas
     dataArrayOT = textscan(fileIDOT, formatSpecOT, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true,  'ReturnOnError', false);
     TempPosition = [str2double(dataArrayOT{1,2}), str2double(dataArrayOT{1,3}), str2double(dataArrayOT{1,4})];
     TempOrient = [str2double(dataArrayOT{1,6}), str2double(dataArrayOT{1,7}), str2double(dataArrayOT{1,8}), str2double(dataArrayOT{1,9})];
-    TimeStamp = str2double(dataArrayOT{1,11});
-    if all(TimeStamp > timeStampDivision)
-        if j==1
-            TimeStampOffsetOT = round(TimeStamp(1) / timeStampDivision);
-        end
-%             TimeStamp = round(TimeStamp / timeStampDivision) - TimeStampOffsetOT;
-            TimeStamp = round(TimeStamp / timeStampDivision);
-    else
-        error('Value of TimeStamp is lower than sampling period')
-    end
+    TimeStampOT = str2double(dataArrayOT{1,11});
+%     if all(TimeStamp > timeStampDivision)
+%         if j==1
+%             TimeStampOffsetOT = round(TimeStamp(1) / timeStampDivision);
+%         end
+% %             TimeStamp = round(TimeStamp / timeStampDivision) - TimeStampOffsetOT;
+%             TimeStamp = round(TimeStamp / timeStampDivision);
+%     else
+%         error('Value of TimeStamp is lower than sampling period')
+%     end
     % Stores OT in the cell
     numPointsOT = size(TempPosition,1)
     if pointsToTake~=0&&numPointsOT>pointsToTake
@@ -69,7 +69,7 @@ for j = 1:numFiles %equals amount of OT-files (each file represents several meas
          if (TempPosition(k,1) < 10000 && TempPosition(k,1) > -10000  && TempPosition(k,2) < 10000 && TempPosition(k,2) > -10000  && TempPosition(k,3) < 10000 && TempPosition(k,3) > -10000)
              dataOT{k-amountErrorPointsOT,1}.position=TempPosition(k,:);
              dataOT{k-amountErrorPointsOT,1}.orientation=TempOrient(k,:);
-             dataOT{k-amountErrorPointsOT,1}.TimeStamp=TimeStamp(k);
+             dataOT{k-amountErrorPointsOT,1}.TimeStamp=TimeStampOT(k);
          else
              amountErrorPointsOT = amountErrorPointsOT + 1;
          end
@@ -82,16 +82,16 @@ for j = 1:numFiles %equals amount of OT-files (each file represents several meas
     TempOrient = [str2double(dataArrayEM{1,8}), str2double(dataArrayEM{1,9}), str2double(dataArrayEM{1,10}), str2double(dataArrayEM{1,11})];
     
     % Split the raw data according to the 3 sensors
-    TimeStamp = str2double(dataArrayEM{1,13});
-    if all(TimeStamp > timeStampDivision)
-        if j==1
-            TimeStampOffsetEM = round(TimeStamp(1) / timeStampDivision);
-        end
-%             TimeStamp = round(TimeStamp / timeStampDivision) - TimeStampOffsetEM;
-            TimeStamp = round(TimeStamp / timeStampDivision);
-    else
-        error('Value of TimeStamp is lower than sampling period')
-    end
+    TimeStampEM = str2double(dataArrayEM{1,13});
+%     if all(TimeStamp > timeStampDivision)
+%         if j==1
+%             TimeStampOffsetEM = round(TimeStamp(1) / timeStampDivision);
+%         end
+% %             TimeStamp = round(TimeStamp / timeStampDivision) - TimeStampOffsetEM;
+%             TimeStamp = round(TimeStamp / timeStampDivision);
+%     else
+%         error('Value of TimeStamp is lower than sampling period')
+%     end
     index0 = 1; index1 = 1; index2 = 1;
     for i = 1:size(TempPosition)
         switch str2double(dataArrayEM{1,2}(i));
@@ -99,7 +99,7 @@ for j = 1:numFiles %equals amount of OT-files (each file represents several meas
             case 0
                 TempPositionFirstSensor(index0,:) = TempPosition(i,:);
                 TempOrientFirstSensor(index0,:) = TempOrient(i,:);
-                TempTimeFirstSensor(index0) = TimeStamp(i);
+                TempTimeFirstSensor(index0) = TimeStampEM(i);
                 index0 = index0 + 1;
         end
     end
@@ -116,27 +116,60 @@ for j = 1:numFiles %equals amount of OT-files (each file represents several meas
             dataEM{k-amountErrorPointsEM,1}.TimeStamp=TempTimeFirstSensor(k);
         else
             amountErrorPointsEM = amountErrorPointsEM + 1;
+            k
         end
     end
 end % Loader
 %% plot loaded timestamps
 numPointsOT = numPointsOT-amountErrorPointsOT
-numPointsEM = numPointsEM-amountErrorPointsEM
+% numPointsEM = numPointsEM-amountErrorPointsEM
 
 comparetimestamps_fighandle = figure;
 hold on
 for k = 1:numPointsOT
-    plot(k, dataOT{k}.TimeStamp/datafreq, 'rx')
+    plot(k, dataOT{k}.TimeStamp, 'rx')
 end
 hold off
 
 hold on
 for k = 1:numPointsEM
-    plot(k+100, dataOT{k}.TimeStamp/datafreq, 'bo')
+    plot(k, TempTimeFirstSensor(k), 'bo')
 end
 hold off
 
+% artificial timestamps for EM sensor
+Art_TimeFirstSensor = linspace(TempTimeFirstSensor(1), TempTimeFirstSensor(end), numPointsOT);
+hold on
+    plot(Art_TimeFirstSensor, 'yo')
+hold off
 
+
+% TimeStampEM_interp = resample(TimeStampEM,length(TimeStampOT),length(TimeStampEM));
+
+comparets_diffs_fighandle = figure;
+hold on
+    plot(TimeStampOT'-Art_TimeFirstSensor)
+%     plot(TimeStampOT'-TimeStampEM_interp', 'y')
+hold off
+%% calc mean sampling frequency
+%change to seconds
+figure
+TimeStampOT = TimeStampOT/1e9;
+diffs_OT = -TimeStampOT(1:end-1)+TimeStampOT(2:end);
+freq_OT = 1./diffs_OT;
+plot(freq_OT)
+hold on
+plot(1:length(freq_OT),mean(freq_OT),'r')
+hold off
+
+figure
+TimeStampEM = TimeStampEM/1e9;
+diffs_EM = -TimeStampEM(1:end-1)+TimeStampEM(2:end);
+freq_EM = 1./diffs_EM;
+plot(freq_EM)
+hold on
+plot(1:length(freq_EM),mean(freq_EM),'r')
+hold off
 
     
     
