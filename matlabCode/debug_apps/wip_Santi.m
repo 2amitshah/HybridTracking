a997ef384c8fe15f653b87b5cb772ffa754e5574
santiagoperez <santiago.perez.chavez@gmail.com>
2013-06-12 16:42:18 +0200

    Function to compute the interval added!

--- matlabCode/debug_apps/wip_Santi.m
+++ matlabCode/debug_apps/wip_Santi.m
@@ -29,5 +29,12 @@ Y = polaris_to_aurora(path, H_OT_to_EMT);
 
 H_EMT_to_EMCSrawcell = trackingdata_to_matrices(data_EMTraw,'CppCodeQuat');
 H_OT_to_OCSrawcell = trackingdata_to_matrices(data_OTraw,'CppCodeQuat');
+interval = obtain_boundaries_for_interpolation(data_OTraw, data_EMTraw);
+data_OT = synthetic_timestamps(data_OTraw,interval,30);
+data_EMT = synthetic_timestamps(data_EMTraw,interval,30);
 
-EMThandle = Plot_points(H_EMT_to_EMCSrawcell);
+H_EMT_to_EMCS = trackingdata_to_matrices(data_EMT,'CppCodeQuat');
+H_OT_to_OCS = trackingdata_to_matrices(data_OT,'CppCodeQuat');
+a = figure;
+subplot(1,2,1), EMTrawhandle = Plot_points(H_EMT_to_EMCSrawcell,a);
+subplot(1,2,2), EMThandle = Plot_points(H_EMT_to_EMCS,a);
---
+++ matlabCode/library/obtain_boundaries_for_interpolation.m
@@ -0,0 +1,19 @@
+function [ interval ] = obtain_boundaries_for_interpolation( dataOT, dataEMT )
+interval = zeros(1,2);
+numSensors = size(dataEMT,2);
+vectorBoundaries = zeros(2,numSensors+1);
+vectorBoundaries(:,1) = [dataOT{1}.TimeStamp; dataOT{end}.TimeStamp];
+matrixHEMT = trackingdata_to_matrices(dataEMT,'CppCodeQuat');
+for i = 1:numSensors
+    pointEndTemp = [];
+    pointEndTemp = find(1 ~= matrixHEMT{i}(4,4,:),1,'first')-1;
+    if isempty(pointEndTemp)
+        pointEndTemp = size(dataEMT,1);
+    end
+    vectorBoundaries(:,i+1) = [dataEMT{1,1}.TimeStamp, dataEMT{pointEndTemp,i}.TimeStamp];
+end
+
+interval = [max(vectorBoundaries(1,:)), min(vectorBoundaries(2,:))];
+
+end
+
